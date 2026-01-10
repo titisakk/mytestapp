6890c2bb8b449d772551b67cfe777ff099579011
titisakk <titisak7795@gmail.com>
2026-01-10 13:40:35 +0700

    Excercise2

--- lib/main.dart
+++ lib/main.dart
@@ -25,30 +25,36 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('First Time App'), backgroundColor: const Color.fromARGB(255, 167, 173, 178), centerTitle: true),
-      body: Center(
-        // child: Container(
-        // height: 250,
-        // width: 250,
-        // padding: EdgeInsets.only(left: 50,top: 30),
-        // decoration:BoxDecoration( 
-        //   color: const Color.fromARGB(255, 26, 20, 142),
-        //   borderRadius: BorderRadius.circular(25)) ,
-        // child: Text('Hello    Thitisak Phanthip 660710593',
-        //   style: TextStyle(
-        //     fontSize: 32,
-        //     fontFamily: 'DroidSans',
-        //     color: Color.fromARGB(255, 72, 226, 221),
-        //     fontWeight: FontWeight.w900,
-        //   ),),),
-        child:Row(mainAxisAlignment: MainAxisAlignment.start,
+      appBar: AppBar(
+        title: Text('First Time App'),
+        backgroundColor: const Color.fromARGB(255, 167, 173, 178),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Expanded(child:Container(height: 50,width: 100,color: Colors.red)),
-          Expanded(flex: 1,child:Container(height: 50,color: const Color.fromARGB(255, 78, 15, 188))),
-          Expanded(flex: 2,child:Container(height: 50,color: const Color.fromARGB(255, 97, 153, 29))),
-          Expanded(flex: 3,child:Container(height: 50,color: const Color.fromARGB(255, 0, 0, 0))),
-        ],
-        )));
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(width: 20),
+              Container(height: 100, width: 100, color: Colors.pink),
+              SizedBox(width: 20),
+              Container(height: 100, width: 100, color: Colors.yellow),
+              SizedBox(width: 20),
+              Container(height: 100, width: 100, color: Colors.blue),
+            ],),
+            SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(width: 20),
+              Expanded(flex: 1,child :Container(height: 100, color: Colors.cyan)),
+              SizedBox(width: 20),
+              Expanded(flex: 2,child :Container(height: 100, color: Colors.black)),
+              SizedBox(width: 20)
+            ],
+          )
+        ]));
   }
-
 }
