4ea33720653626ca359c84d260c0b89dc0df9c77
titisakk <titisak7795@gmail.com>
2026-02-14 02:15:25 +0700

    week3.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mytestapp/screen/content_screen.dart';
-import 'package:mytestapp/screen/greeting_screen.dart';
-import 'package:mytestapp/screen/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,22 +10,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter First Project',
-      debugShowCheckedModeBanner: false,
-      // initialRoute: '/greeting',
-      // routes: {
-      //   '/greeting':(context) => GreetingScreen(name: 'Thitisak',
-      //   bgColor: Colors.blue,),
-      // '/content':(context) => ContentScreen()
-      // },
-      
-      theme: ThemeData(
-        // Added ColorScheme class name here
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
-        useMaterial3: true,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Grid Layout"),
+        ),
+        body:  Center(
+          child: Text("Hello"),
+        ),
       ),
-      home: BottomNavigatorExample()
     );
   }
 }
-
