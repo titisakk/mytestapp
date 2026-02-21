f6471ab57fe9555048822bcb27543df7490101ac
titisakk <titisak7795@gmail.com>
2026-02-21 10:40:28 +0700

    listview api without model class.

--- lib/screen/list_user_screen.dart
+++ lib/screen/list_user_screen.dart
@@ -1,4 +1,4 @@
-import 'dart:convert';
+mport 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -7,35 +7,48 @@ class ListUserScreen extends StatefulWidget {
   const ListUserScreen({super.key});
 
   @override
-  State<ListUserScreen> createState() => _ListUserScreen();
+  State<ListUserScreen> createState() => _ListUserScreenState();
 }
 
-class _ListUserScreen extends State<ListUserScreen> {
-
+class _ListUserScreenState extends State<ListUserScreen> {
+  List<dynamic> listUser = [];
   void getUser() async {
     try {
-      var response = await http.get(Uri.parse('https://dummyjson.com/users'));
+      var response = await http.get(Uri.parse('https://dummyjson.com/user'));
       if (response.statusCode == 200) {
-        print(response.body);
-        var data =jsonDecode(response.body);
+        var data = jsonDecode(response.body);
         List<dynamic> jsonList = data['users'];
-        print(jsonList[0]);
+        setState(() {
+          listUser = jsonList;
+        });
       }
     } catch (e) {
       print('Error : $e');
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Call Api')),
-      body: Column(children: [
-        ElevatedButton(onPressed: (){
-          getUser();
-        }, child: Text('Call Api'))
-      ],
-
+      appBar: AppBar(
+        title: Text('Call API'),
       ),
+      body: ListView.separated(
+        itemCount: listUser.length,
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            leading: Text('ID ${listUser[index]['id']}'),
+            title: Text(
+                '${listUser[index]['firstName']} ${listUser[index]['lastName']}'),
+          );
+        },
+      ),
+      floatingActionButton: ElevatedButton(
+          onPressed: () {
+            getUser();
+          },
+          child: Text('Call API')),
     );
   }
 }
