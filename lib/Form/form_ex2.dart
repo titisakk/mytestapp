81c1f2491a1d22f317e4c5b77efe2755824838c1
titisakk <titisak7795@gmail.com>
2026-03-04 01:45:44 +0700

    form2

---
+++ lib/Form/form_ex2.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class FormEx2 extends StatefulWidget {
+  const FormEx2({super.key});
+
+  @override
+  State<FormEx2> createState() => _FormEx2State();
+}
+
+class _FormEx2State extends State<FormEx2> {
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>;
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/Form/form_excerszie.dart
+++ lib/Form/form_excerszie.dart
@@ -8,7 +8,12 @@ class FormExcerszie extends StatefulWidget {
 }
 
 class _FormExcerszieState extends State<FormExcerszie> {
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
   String strInput = '';
+  // textediting controller
+  final TextEditingController _userController =TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,38 +23,65 @@ class _FormExcerszieState extends State<FormExcerszie> {
         backgroundColor: Colors.blue,
         centerTitle: true,
       ),
-      body: Column(crossAxisAlignment: CrossAxisAlignment.start,
+      body: Form(
+        key: _formkey,
+        child: Column(crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Padding(padding: EdgeInsetsGeometry.only(top: 20)),
-          Text('ชื่อ',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+          Text('Display Input : $strInput',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
           SizedBox(height: 10,),
           TextFormField(
             decoration: InputDecoration(
               border: OutlineInputBorder(),
               labelText: 'Username'),
-            validator: (String? value) {},
-            onChanged: (String value) {
-              setState(() {
-                strInput = value;
-              });
-            },),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'กรุณากรอกข้อมูล';
+              }
+              return null;
+            },
+            // onChanged: (String value) {
+            //   setState(() {
+            //     strInput = value;
+            //   });
+            // },
+            ),
+
           SizedBox(height: 20,),
 
-
-          Padding(padding: EdgeInsetsGeometry.only(top: 20)),
-          Text('รหัสผ่าน',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+          Padding(padding: EdgeInsets.only(top: 20)),
           SizedBox(height: 10,),
           TextFormField(
             obscureText: true,
             decoration: InputDecoration(
               border: OutlineInputBorder(),
               labelText: 'Password'),
-            validator: (String? value) {},
-            onChanged: (String value) {
-              setState(() {
-                strInput = value;
-              });
-            },),
-        ],),);
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'กรุณากรอกข้อมูล';
+              }
+              return null;
+            },
+            // onChanged: (String value) {
+            //   setState(() {
+            //     strInput = value;
+            //    });
+            // },
+            ),
+
+            ElevatedButton(onPressed: () {
+              if (_formkey.currentState!.validate()) {
+                setState(() {
+                  strInput = 'Form is valid';
+                });
+              }
+              else {
+               setState(() {
+                 strInput = 'Form is Invalid';
+               });
+              }
+            }, child: Text('ลงชื่อเข้าใช้'))
+        ],),));
   }
 }
